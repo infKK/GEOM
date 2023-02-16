3ceeec55c6bf85f7ebec004d2ce8e47c79de5d5c
khlebnovaea.24 <toscana311@mail.ru>
2023-02-16 11:16:52 +0300

    in progress

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -19,4 +19,13 @@ public class Colors {
         throw new AssertionError("Вызов этого конструктора запрещён");
     }
 
+    /**
+     * Цвет текста заголовка
+     */
+    public static final int LABEL_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
+    /**
+     * цвет подложки панелей
+     */
+    public static final int PANEL_BACKGROUND_COLOR = Misc.getColor(32, 0, 0, 0);
+
 }
