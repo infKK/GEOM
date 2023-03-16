7734891274f503409f91f847cd3a89755c34743e
khlebnovaea.24 <toscana311@mail.ru>
2023-03-16 12:34:37 +0300

    control beginning

--- src/main/java/panels/PanelControl.java
+++ src/main/java/panels/PanelControl.java
@@ -142,7 +142,39 @@ public class PanelControl extends GridPanel {
                 PanelRendering.task.addRandomPoints(cntField.intValue());
         });
         buttons.add(addPoints);
+// управление
+        Button load = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 0, 5, 3, 1, "Загрузить",
+                true, true);
+        load.setOnClick(() -> {
+            PanelRendering.load();
+            cancelTask();
+        });
+        buttons.add(load);
 
+        Button save = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 3, 5, 3, 1, "Сохранить",
+                true, true);
+        save.setOnClick(PanelRendering::save);
+        buttons.add(save);
+
+        Button clear = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 0, 6, 3, 1, "Очистить",
+                true, true);
+        clear.setOnClick(() -> PanelRendering.task.clear());
+        buttons.add(clear);
+
+        solve = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 3, 6, 3, 1, "Решить",
+                true, true);
+        solve.setOnClick(() -> {
+            PanelRendering.task.solve();
+        });
+        buttons.add(solve);
     }
 
     /**
@@ -228,4 +260,16 @@ public class PanelControl extends GridPanel {
             label.paint(canvas, windowCS);
         }
     }
+    /**
+     * Сброс решения задачи
+     */
+    private void cancelTask() {
+        PanelRendering.task.cancel();
+        // Задаём новый текст кнопке решения
+        solve.text = "Решить";
+    }
+    /**
+     * Кнопка "решить"
+     */
+    private final Button solve;
 }
